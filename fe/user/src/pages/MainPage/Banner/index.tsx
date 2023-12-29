ec9267e6f5b12905270102edd98bd2c2289e885c
jsh3418 <albert5428@gmail.com>
2023-12-29 16:06:17 +0900

    fix: 배너 이미지 url 수정

--- fe/user/src/pages/MainPage/Banner/index.tsx
+++ fe/user/src/pages/MainPage/Banner/index.tsx
@@ -6,11 +6,11 @@ export const Banner: React.FC = () => {
     <StyledBanner>
       <SwiperComponent
         imageUrls={[
-          'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/85b44f33-49b9-46d6-80de-e9a538c44b2f',
-          'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/dc2bf706-ec24-4e5b-b741-467f91ae8618',
-          'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/f51d660d-a2ba-4ca9-9dc3-84214eb37ae5',
-          'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/51a4d669-a2e7-4a90-b4d4-af4e9c63dd0d',
-          'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/0ee2e38f-0e63-498d-963d-8f1abe5239a0'
+          'https://jazzmeet.s3.ap-northeast-2.amazonaws.com/banner/banner_1.webp',
+          'https://jazzmeet.s3.ap-northeast-2.amazonaws.com/banner/banner_2.webp',
+          'https://jazzmeet.s3.ap-northeast-2.amazonaws.com/banner/banner_3.webp',
+          'https://jazzmeet.s3.ap-northeast-2.amazonaws.com/banner/banner_4.webp',
+          'https://jazzmeet.s3.ap-northeast-2.amazonaws.com/banner/banner_5.webp',
         ]}
       />
     </StyledBanner>
